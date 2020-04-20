7a96e14386d2a013294cbd400883fd4152beed53
Andres <aneelm@ttu.ee>
2020-04-20 19:07:46 +0300

    Removed unnecessary methods from controller

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -40,79 +40,11 @@ namespace CGIBack.Controllers
             return _movieService.GetMoviesByTitle(title);
         }
 
-        // GET: Movies/filter
+        // GET: Movies/filter/?categorieIds=1,2,3
         public List<MovieDto> Filter(string categorieIds)
         {
             return _movieService.GetMoviesByCategoryId(categorieIds);
         }
             
-        // GET: Movies/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Movies/Create
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Movies/Edit/5
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        // POST: Movies/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Movies/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Movies/Delete/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction(nameof(Index));
-            }
-            catch
-            {
-                return View();
-            }
-        }
     }
 }
